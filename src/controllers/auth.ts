089c779953014862e2ff4d2cd89ffd3ee44c8b1b
Ghayas <ghayasleo99@gmail.com>
2025-05-04 00:40:04 +0500

    Added userdata in signup

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -126,6 +126,8 @@ export const signup = catchAsync(async (req, res, next) => {
   }
 
   const user = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
